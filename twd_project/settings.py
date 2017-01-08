0135e5a62c27672e854d2f31aea100dadab881b4
Adam <aweaver79au@gmail.com>
2017-01-08 21:01:05 +1030

    twd_project root folder tidied up and Rango APP created

--- twd_project/settings.py
+++ twd_project/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'twd_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3', or 'oracle'
+        'NAME': 'twd',
+        # The following settings are not used with sqlite3:
+        'USER': 'adam',
+        'PASSWORD': 'password',
+        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.        
+        'PORT': '',  # Set to empty string for default.
     }
 }
 
