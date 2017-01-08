9f34373e96c6a3996d767502fc55a087d6d2e04c
Adam <aweaver79au@gmail.com>
2017-01-08 21:55:09 +1030

    Changed database settings to use host 127.0.0.1 instead of localhost

--- twd_project/settings.py
+++ twd_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'adam',
         'PASSWORD': 'password',
-        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.        
+        'HOST': '127.0.0.1',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.        
         'PORT': '',  # Set to empty string for default.
     }
 }
