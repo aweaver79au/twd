04f73cd1dea6f434c7c353ba47ce83aa2d1c7f50
Adam <aweaver79au@gmail.com>
2017-01-09 21:01:15 +1030

    Added about page for rango App

--- rango/views.py
+++ rango/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Rango says hey there partner")
 
+def about(request):
+    return HttpResponse("Rango says here's the about page")
+
